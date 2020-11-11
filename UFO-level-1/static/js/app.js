6f251cf632a54027ed13163af2a32dac61c5b2fa
Ronald Rivas <ronaldrivas@Ronalds-MacBook-Pro.local>
2020-11-11 12:11:45 -0600

    Creating a filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,8 +5,20 @@ const tableData = data;
 // Connect directly to body of table
 const tbody = d3.select("tbody");
 
+// Filtering data using date
+function clickFilter({
+    const date = d3.select("#datetime").property("value");
+    let filterInfo = tableData;
+}
+
+)
+
 function createTable(info {
     tbody.html("");
 
-    info.forEach
+    info.forEach((dataInfo) => {
+
+    }
+
+    )
 }
